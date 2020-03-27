a39461787413bf8cc9b2cde2c2e060f7106b06b8
Notani <michaelshin224@yahoo.co.jp>
2020-03-27 12:33:28 +0000

    phpkadai14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\profile;
+
 class ProfileController extends Controller
 {
   // add　プロフィールの新規作成画面を表示するアクション。
@@ -13,27 +15,33 @@ class ProfileController extends Controller
     return view('admin.profile.create');
   }
   
-  // create 入力されたプロフィールをデータベースに保存するアクション。
-  public function create()
+  public function create(Request $request)
   {
-    return redirect ('admin/profile/create');
-  }
-  
-  // edit 既に登録されているプロフィールの編集画面を表示するアクション。
-  public function edit()
-  {
-    // viewとは、resources/views/admin/profile/edit.blade.php を、画面に変換して返す。
-    return view('admin.profile.edit');
-  }
-  
-  // update 入力されたプロフィール情報で、データベースを更新するアクション。
-  public function update()
-  {
-    // redirectとは、指定したURLをRoutingに渡し、対応するアクションを起動する。
-    return redirect('admin/profile/edit');
-  }
-  
-  
-}
 
-// redirectとは、Routingに処理を戻す。指定したURLに対応するアクションを起動する。
+      // 以下を追記
+      // Varidationを行う
+      $this->validate($request, profile::$rules);
+
+      $news = new News;
+      $form = $request->all();
+
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news->image_path = basename($path);
+      } else {
+          $news->image_path = null;
+      }
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
+
+      return redirect('admin/profile/create');
+  }
+}
